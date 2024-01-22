d510fd8fc5180a5ac1eaf53a3f112048a355b169
stevensikorski <ssikorski03@gmail.com>
2024-01-21 21:37:53 -0500

    Progress on header and other design components

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,22 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  content: ["./src/pages/**/*.{js,ts,jsx,tsx,mdx}", "./src/components/**/*.{js,ts,jsx,tsx,mdx}", "./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        ebgaramond: ["--font-ebgaramond"],
+        merriweather: ["--font-merriweather"],
+        roboto: ["--font-roboto"],
+      },
+      colors: {
+        czerwony: "#DC143C",
+        primary: "#09090b",
+        secondary: "#52525b",
+        tertiary: "#a1a1aa",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
