4cd1f7619f1222be7c2f81936a34974565b5a61a
stevensikorski <ssikorski03@gmail.com>
2024-07-03 23:18:55 -0400

    removed caching

--- src/app/api/events/route.ts
+++ src/app/api/events/route.ts
@@ -1,27 +1,13 @@
 import { NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
-import NodeCache from "node-cache";
 import { getServerSession } from "next-auth/next";
 import { authOptions } from "@/lib/options";
 
-const cache = new NodeCache({ stdTTL: 3600 });
-
 export async function GET(request: Request) {
-  const key = "events";
-
-  const cachedEvents = cache.get(key);
-  if (cachedEvents) {
-    console.log("Using cached data");
-    return NextResponse.json({ events: cachedEvents });
-  }
-
   const events = await prisma.event.findMany();
   if (!events) {
-    console.log("Error");
     return new NextResponse(JSON.stringify({ error: "empty" }), { status: 404 });
   }
-  cache.set(key, events);
-  console.log("Using prisma data");
   return NextResponse.json({ events });
 }
 
@@ -53,12 +39,6 @@ export async function POST(request: Request) {
     },
   });
 
-  const key = "events";
-  const cachedEvents = cache.get(key);
-  if (cachedEvents) {
-    cache.del(key);
-  }
-
   return NextResponse.json({
     event: {
       id: event.id,
@@ -78,15 +58,9 @@ export async function DELETE(request: Request) {
     return new NextResponse(JSON.stringify({ error: "id is required" }), { status: 400 });
   }
 
-  const event = await prisma.event.delete({
+  await prisma.event.delete({
     where: { id },
   });
-
-  const key = "events";
-  const cachedEvents = cache.get(key);
-  if (cachedEvents) {
-    cache.del(key);
-  }
 
   return NextResponse.json({ message: "Event deleted successfully" });
 }
